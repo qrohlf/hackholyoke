d6278c88a6ef8ddb587bd9b7f4a2d42718ca0ea4
Quinn Rohlf <qr@qrohlf.com>
2015-04-04 07:29:22 -0400

    fix CORS

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + "/dist"))
 // Express config: Fuck security
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
  });
 
